ee93bbc5924550a93c2013caf6107877fc0934ea
SWE-Jahidul <programmerjahid162@gmail.com>
2021-10-18 13:13:13 +0600

    adding my route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Component/Home/Home";
+import About from "./Component/About/About";
+import Services from "./Component/Services/Services";
+import Footer from "./Component/Footer/Footer";
+import Doctors from "./Component/Doctors/Doctors";
+import Contact from "./Component/Contact/Contact";
+import Signin from "./Component/Signin/Signin";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+
+          <Route path="/about">
+            <About></About>
+          </Route>
+
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+
+          <Route path="/doctors">
+            <Doctors></Doctors>
+          </Route>
+
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+
+          <Route path="/singin">
+            <Signin></Signin>
+          </Route>
+        </Switch>
+
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="row pt-4">
+        <div
+          className="col-md-6 fw-bold "
+          style={{
+            fontSize: 25,
+          }}
+        >
+          MEDICARE
+        </div>
+        <div className="col-md-6">
+          <span>Location : Bhandaria, Pirojpur </span>
+
+          <span
+            className="ms-2"
+            style={{
+              color: "#84D6FF",
+            }}
+          >
+            Contact :017123456789
+          </span>
+        </div>
+      </div>
+
+      <nav className="navbar navbar-expand-lg ">
+       
+    
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon ">
+              <i class="fa fa-navicon" aria-hidden="true"></i>
+            </span>
+          </button>
+          <div
+            className="collapse navbar-collapse fw-bold"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <li className="nav-item">
+                <Link to="/" className="nav-link menu-link">
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/about" className="nav-link menu-link">
+                  About
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/services" className="nav-link menu-link">
+                  Services
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link menu-link">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/doctors" className="nav-link menu-link">
+                  Doctors
+                </Link>
+              </li>
+            </ul>
+        </div>
+
+        <div className="d-flex flex-row-reverse ">
+          <Link to="/singin" className="signin " >
+            Sign In
+          </Link>
+          <Link to="/singout" className="Singout me-3">
+            Sign Out
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
